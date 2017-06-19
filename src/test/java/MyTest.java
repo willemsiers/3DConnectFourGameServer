c501f61e4554529c3367b9d044c519b0143679f0
willem <willemhsiers@gmail.com>
2017-06-19 13:01:12 +0200

    Add Unit test

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,25 @@
+import game.Board;
+import game.GridMark;
+import game.Move;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void testPrintHelloWorld() {
+
+		Board board = new Board();
+		Move move = new Move(4, 0, GridMark.RED);
+		System.out.println(board.isValidMove(move));
+		board.makeMove(new Move(0, 0, GridMark.RED));
+		board.makeMove(new Move(0, 0, GridMark.RED));
+		board.makeMove(new Move(0, 0, GridMark.RED));
+		board.makeMove(new Move(1, 2, GridMark.YELLOW));
+		Move last = new Move(0, 0, GridMark.RED);
+		board.makeMove(last);
+		Assert.assertTrue(board.isWinner(last));
+
+	}
+
+}
